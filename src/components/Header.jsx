e53591e52b9b3bf1c91f3315c940980fb11d1a60
nischala <nischala.manjunath@gmail.com>
2024-03-08 23:59:25 -0800

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import DrawOutlinedIcon from '@mui/icons-material/DrawOutlined';
+
+function Header() {
+  return (
+    <header>
+      <h1><DrawOutlinedIcon />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
